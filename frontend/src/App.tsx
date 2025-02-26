be2307546d69023f52ce9c06f9ad03224b636800
Shubham <shubhampatil1356@gmail.com>
2025-02-26 18:35:27 +0530

    initialize backend and frontend setup (Node.js, Express, React, Tailwind)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-blue-600 text-center mt-24'>
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
