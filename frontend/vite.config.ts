72552a9f5e25fe010ec22db09117890b2410f98a
Shubham Patil <106343332+Shubham-567@users.noreply.github.com>
2025-03-01 00:32:16 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      'task-management-app-frontend-si04.onrender.com', // Add Render host
+      'localhost',  // Allow localhost for local development
+    ],
+  },
 });
